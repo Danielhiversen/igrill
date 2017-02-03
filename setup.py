3ef77d611cef8e8edf2cb6ef9cc8d6e995ce485c
Daniel Hoyer Iversen <Daniel.H.Iversen@ntnu.no>
2017-02-03 15:23:44 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+'''
+
+'''
+
+from setuptools import setup
+
+setup(
+    name = 'igrill',
+    packages = ['igrill'],
+    version = '0.1.0',
+    url='https://github.com/Danielhiversen/igrill',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+)
